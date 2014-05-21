44512b3cd2682ce4c882c504b27ff7db60242e1f
Winston Chang <winston@stdout.org>
2014-05-21 12:18:09 -0500

    Remove unused function

--- R/env_utils.R
+++ R/env_utils.R
@@ -1,8 +1,3 @@
-# Copy all objects from one environment to another
-copy_env <- function(src, dest = new.env(parent = emptyenv())) {
-  list2env(as.list.environment(src, all.names = TRUE), envir = dest)
-}
-
 # Search a list for all function objects, change the environment for those
 # functions to a target environment, and return the modified list.
 assign_func_envs <- function(objs, target_env) {
