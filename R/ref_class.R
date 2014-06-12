c535d4104620d0acdfb2da24e2eb11c19af158b6
Winston Chang <winston@stdout.org>
2014-06-12 01:22:04 -0500

    Add support for single-level inheritance

--- R/ref_class.R
+++ R/ref_class.R
@@ -61,6 +61,8 @@
 #' @param private An optional list of private members, which can be functions
 #'   and non-functions.
 #' @param active An optional list of active binding functions.
+#' @param inherit A RefClassGenerator object to inherit from; in other words,
+#'   a superclass for this class.
 #' @param parent_env An environment to use as the parent of newly-created
 #'   objects.
 #' @param class Should a class attribute be added to the public environment?
@@ -148,10 +150,41 @@
 #'
 #' # Print, using the print.RefClass method:
 #' print(z)
+#'
+#'
+#'
+#' # Inheritance
+#'
 createRefClass <- function(classname = NULL, public = list(),
                            private = NULL, active = NULL,
-                           parent_env = parent.frame(), lock = TRUE,
-                           class = TRUE) {
+                           inherit = NULL, parent_env = parent.frame(),
+                           lock = TRUE, class = TRUE) {
+
+  if (!all_named(public) || !all_named(private) || !all_named(active)) {
+    stop("All elements of public, private, and active must be named.")
+  }
+  if (any(duplicated(c(names(public), names(private), names(active))))) {
+    stop("All items in public, private, and active must have unique names.")
+  }
+  if (length(get_nonfunctions(active)) != 0) {
+    stop("All items in active must be functions.")
+  }
+
+  if (!is.null(inherit)) {
+    if (!inherits(inherit, "RefClassGenerator")) {
+      stop("`inherit` must be a RefClass.")
+    }
+
+    # Merge the new items over the inherited ones
+    public  <- merge_vectors(inherit$public,  public)
+    private <- merge_vectors(inherit$private, private)
+    active  <- merge_vectors(inherit$active,  active)
+
+    # Get just the functions from the inherited class.
+    # These will be placed in the super env.
+    super_funs <- c(get_functions(inherit$public), get_functions(inherit$private))
+    super_active <- inherit$active
+  }
 
   has_private <- !is.null(private)
 
@@ -179,12 +212,35 @@ createRefClass <- function(classname = NULL, public = list(),
       public_env$private <- private_env
     }
 
+    # Set up active bindings
     if (!is.null(active)) {
       active <- assign_func_envs(active, public_env)
 
       for (name in names(active)) {
         makeActiveBinding(name, active[[name]], public_env)
       }
+    }
+
+    # Set up super environment, which contains just the functions from the
+    # inherited class.
+    if (!is.null(inherit) &&
+        (!is.null(super_funs) || !is.null(super_active))) {
+      super_env <- new.env(parent = emptyenv(),
+        hash = length(super_funs) + length(super_active) > 100)
+
+      if (!is.null(super_funs)) {
+        super_funs <- assign_func_envs(super_funs, public_env)
+        list2env(super_funs, envir = super_env)
+      }
+
+      if (!is.null(super_active)) {
+        super_active <- assign_func_envs(super_active, public_env)
+        for (name in names(super_active)) {
+          makeActiveBinding(name, super_active[[name]], super_env)
+        }
+      }
+
+      public_env$super <- super_env
     }
 
     if (lock) {
@@ -199,8 +255,20 @@ createRefClass <- function(classname = NULL, public = list(),
 
   structure(
     list(new = newfun, classname = classname, public = public,
-         private = private, active = active, parent_env = parent_env,
-         lock = lock),
+         private = private, active = active, inherit = inherit,
+         parent_env = parent_env, lock = lock),
     class = "RefClassGenerator"
   )
+}
+
+get_functions <- function(x) {
+  funcs <- vapply(x, is.function, logical(1))
+  if (all(!funcs)) return(NULL)
+  x[funcs]
+}
+
+get_nonfunctions <- function(x) {
+  funcs <- vapply(x, is.function, logical(1))
+  if (all(funcs)) return(NULL)
+  x[!funcs]
 }
