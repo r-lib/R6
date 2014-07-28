360552552f87ec0b8e3756c32525a700d5e29391
Winston Chang <winston@stdout.org>
2014-07-27 21:33:47 -0500

    Printing tweaks

--- R/print.R
+++ R/print.R
@@ -42,8 +42,9 @@ print.R6ClassGenerator <- function(x, ...) {
       sep = ""
     )
   }
-  cat("\n  Parent: ", format(x$parent_env),  sep = "")
+  cat("\n  Parent env: ", format(x$parent_env),  sep = "")
   cat("\n  Lock: ", x$lock,  sep = "")
+  cat("\n  Modular: ", x$modular,  sep = "")
 }
 
 # Return a summary string of the items of a list or environment
