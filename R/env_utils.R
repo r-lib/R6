012416379d94bf50e2f2f2af55c77c83b2850d56
Winston Chang <winston@stdout.org>
2014-08-12 21:25:41 -0500

    Encapsulate functions involved in instantiating objects

--- R/env_utils.R
+++ R/env_utils.R
@@ -1,35 +1,37 @@
-# Search a list for all function objects, change the environment for those
-# functions to a target environment, and return the modified list.
-assign_func_envs <- function(objs, target_env) {
-  if (is.null(target_env)) return(objs)
+encapsulate({
+  # Search a list for all function objects, change the environment for those
+  # functions to a target environment, and return the modified list.
+  assign_func_envs <- function(objs, target_env) {
+    if (is.null(target_env)) return(objs)
 
-  lapply(objs, function(x) {
-    if (is.function(x)) environment(x) <- target_env
-    x
-  })
-}
-
-# Get names of all superclasses
-get_superclassnames <- function(inherit) {
-  if (is.null(inherit)) return(NULL)
-  c(inherit$classname, get_superclassnames(inherit$get_inherit()))
-}
-
-# Wrapper around list2env with a NULL check
-# @param empty_to_null Controls what to do when x is NULL or empty list.
-#   If TRUE, return NULL. If FALSE, return an empty list.
-list2env2 <- function(x, envir = NULL, parent = emptyenv(),
-                      hash = (length(x) >  100),
-                      size = max(29L, length(x)),
-                      empty_to_null = TRUE) {
-  if (is.null(envir)) {
-    envir <- new.env(hash = hash, parent = parent, size = size)
+    lapply(objs, function(x) {
+      if (is.function(x)) environment(x) <- target_env
+      x
+    })
   }
-  if (length(x) == 0) {
-    if (empty_to_null)
-      return(NULL)
-    else
-      return(envir)
+
+  # Get names of all superclasses
+  get_superclassnames <- function(inherit) {
+    if (is.null(inherit)) return(NULL)
+    c(inherit$classname, get_superclassnames(inherit$get_inherit()))
   }
-  list2env(x, envir)
-}
+
+  # Wrapper around list2env with a NULL check
+  # @param empty_to_null Controls what to do when x is NULL or empty list.
+  #   If TRUE, return NULL. If FALSE, return an empty list.
+  list2env2 <- function(x, envir = NULL, parent = emptyenv(),
+                        hash = (length(x) >  100),
+                        size = max(29L, length(x)),
+                        empty_to_null = TRUE) {
+    if (is.null(envir)) {
+      envir <- new.env(hash = hash, parent = parent, size = size)
+    }
+    if (length(x) == 0) {
+      if (empty_to_null)
+        return(NULL)
+      else
+        return(envir)
+    }
+    list2env(x, envir)
+  }
+})
--- R/utils.R
+++ R/utils.R
@@ -1,40 +1,56 @@
-# Given two named vectors, join them together, and keep only the last element
-# with a given name in the resulting vector. If b has any elements with the
-# same name as elements in a, the element in a is dropped. Also, if there are
-# any duplicated names in a or b, only the last one with that name is kept.
-merge_vectors <- function(a, b) {
-  if ((!is.null(a) && length(a) > 1 && is.null(names(a))) ||
-      (!is.null(b) && length(b) > 1 && is.null(names(b)))) {
-    stop("merge_vectors: vectors must be either NULL or named vectors")
+# This is the enclosing environment for all of the functions involved in
+# instantiating objects. It is also the binding environment for all these
+# functions, except for R6Class(). This is because a generator object can be
+# saved (in a built package, for example) and then restored in a different R
+# session which has a different version of the R6 package. With the capsule
+# environment, the generator object doesn't need to use any functions or objects
+# from the potentially different R6 namespace, and because the saved/restored
+# object also saves and restores the capsule environment (but not the R6
+# namespace).
+capsule <- new.env(hash = FALSE)
+
+# This function takes an expression and evaluates it in the capsule environment.
+encapsulate <- function(expr) {
+  expr <- substitute(expr)
+  eval(expr, capsule)
+}
+
+encapsulate({
+  # Given two named vectors, join them together, and keep only the last element
+  # with a given name in the resulting vector. If b has any elements with the
+  # same name as elements in a, the element in a is dropped. Also, if there are
+  # any duplicated names in a or b, only the last one with that name is kept.
+  merge_vectors <- function(a, b) {
+    if ((!is.null(a) && length(a) > 1 && is.null(names(a))) ||
+        (!is.null(b) && length(b) > 1 && is.null(names(b)))) {
+      stop("merge_vectors: vectors must be either NULL or named vectors")
+    }
+
+    x <- c(a, b)
+    drop_idx <- duplicated(names(x), fromLast = TRUE)
+    x[!drop_idx]
   }
 
-  x <- c(a, b)
-  drop_idx <- duplicated(names(x), fromLast = TRUE)
-  x[!drop_idx]
-}
-
-
-# Check that all elements of a list are named.
-# NULL and empty lists return TRUE.
-all_named <- function(x) {
-  if (length(names(x)) != length(x) || any(names(x) == "")) {
-    return(FALSE)
+  # Check that all elements of a list are named.
+  # NULL and empty lists return TRUE.
+  all_named <- function(x) {
+    if (length(names(x)) != length(x) || any(names(x) == "")) {
+      return(FALSE)
+    }
+    TRUE
   }
-  TRUE
-}
 
+  # Return all the functions in a list.
+  get_functions <- function(x) {
+    funcs <- vapply(x, is.function, logical(1))
+    if (all(!funcs)) return(NULL)
+    x[funcs]
+  }
 
-# Return all the functions in a list.
-get_functions <- function(x) {
-  funcs <- vapply(x, is.function, logical(1))
-  if (all(!funcs)) return(NULL)
-  x[funcs]
-}
-
-
-# Return all the non-functions in a list.
-get_nonfunctions <- function(x) {
-  funcs <- vapply(x, is.function, logical(1))
-  if (all(funcs)) return(NULL)
-  x[!funcs]
-}
+  # Return all the non-functions in a list.
+  get_nonfunctions <- function(x) {
+    funcs <- vapply(x, is.function, logical(1))
+    if (all(funcs)) return(NULL)
+    x[!funcs]
+  }
+})
