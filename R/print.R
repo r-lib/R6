822f029dd457f8308e6dcc43d72565b87fb48538
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-09-19 16:03:42 +0200

    Override format() for R6 and R6ClassGenerator
    - R6.format() calls format() method of the same name if it exists
    - object_summaries now returns a character vector, one per line
    - no testthat tests
    - R6 objects now look better in the "Format" section generated by roxygen2

--- R/print.R
+++ R/print.R
@@ -1,71 +1,78 @@
 #' @export
-print.R6 <- function(x, ...) {
-  if (is.function(x$print)) {
-    x$print(...)
-
+format.R6 <- function(x, ...) {
+  if (is.function(x$format)) {
+    x$format(...)
   } else {
-    cat(
-      "<", class(x)[1], ">\n",
-      "  Public:\n",
-      indent(object_summaries(x, exclude = ".__enclos_env__"), 4),
-      "\n",
-      sep = ""
+    ret <- c(
+      paste0("<", class(x)[1], ">"),
+      "  Public:",
+      indent(object_summaries(x, exclude = ".__enclos_env__"), 4)
     )
 
     private <- x$`.__enclos_env__`$private
     if (!is.null(private)) {
-      cat(
-        "  Private:\n",
-        indent(object_summaries(private), 4),
-        "\n",
-        sep = ""
+      ret <- c(ret,
+        "  Private:",
+        indent(object_summaries(private), 4)
       )
     }
-    invisible(x)
+    paste(ret, collapse = "\n")
   }
 }
 
 #' @export
-print.R6ClassGenerator <- function(x, ...) {
+print.R6 <- function(x, ...) {
+  if (is.function(x$print)) {
+    x$print(...)
+  } else {
+    cat(format(x, ...), sep = "\n")
+  }
+}
+
+#' @export
+format.R6ClassGenerator <- function(x, ...) {
   classname <- x$classname
   if (is.null(classname)) classname <- "unnamed"
-  cat(
-    "<", classname, "> object generator\n",
-    "  Public:\n",
+  ret <- c(
+    paste0("<", classname, "> object generator"),
+    "  Public:",
     indent(object_summaries(x$public_fields), 4),
-    indent(object_summaries(x$public_methods), 4),
-    sep = ""
+    indent(object_summaries(x$public_methods), 4)
   )
 
   if (!is.null(x$active)) {
-    cat(
-      "  Active bindings:\n",
-      indent(object_summaries(x$active), 4),
-      sep = ""
+    ret <- c(ret,
+      "  Active bindings:",
+      indent(object_summaries(x$active), 4)
     )
   }
 
   if (!(is.null(x$private_fields) && is.null(x$private_methods))) {
-    cat(
-      "  Private:\n",
+    ret <- c(ret,
+      "  Private:",
       indent(object_summaries(x$private_fields), 4),
-      indent(object_summaries(x$private_methods), 4),
-      sep = ""
+      indent(object_summaries(x$private_methods), 4)
     )
   }
-  cat("  Parent env: ", format(x$parent_env), "\n", sep = "")
+  ret <- c(ret, paste("  Parent env:", format(x$parent_env)))
   # R6 generators created by versions <2.1 could be used with this version of
   # print. They had x$lock instead of x$lock_objects, and they didn't have
   # x$lock_class at all. Make sure we don't error in that case. Eventually we'll
   # be able to remove this check.
   if (!is.null(x$lock) && is.logical(x$lock))
-    cat("  Locked objects: ", x$lock, "\n", sep = "")
+    ret <- c(ret, paste("  Locked objects:", x$lock))
   if (!is.null(x$lock_objects))
-    cat("  Locked objects: ", x$lock_objects, "\n", sep = "")
+    ret <- c(ret, paste("  Locked objects:", x$lock_objects))
   if (!is.null(x$lock_class))
-    cat("  Locked class: ", x$lock_class, "\n", sep = "")
-  cat("  Portable: ", x$portable, "\n", sep = "")
-  invisible(x)
+    ret <- c(ret, paste("  Locked class:", x$lock_class))
+  ret <- c(ret, paste("  Portable:", x$portable))
+
+  paste(ret, collapse = "\n")
+}
+
+#' @export
+print.R6ClassGenerator <- function(x, ...) {
+  cat(format(x, ...), sep = "\n")
 }
 
 # Return a summary string of the items of a list or environment
@@ -94,10 +101,7 @@ object_summaries <- function(x, exclude = NULL) {
     }
   }, FUN.VALUE = character(1))
 
-  paste0(
-    paste0(obj_names, ": ", values, sep = "", collapse = "\n"),
-    "\n"
-  )
+  paste0(obj_names, ": ", values, sep = "")
 }
 
 # Given a string, indent every line by some number of spaces.
