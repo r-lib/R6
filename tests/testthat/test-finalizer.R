77aa00a34a66a71c859649e0b7f2acc401680642
Gábor Csárdi <csardi.gabor@gmail.com>
2016-08-22 21:04:37 +0100

    Test cases for finalizers vs inheritance

--- tests/testthat/test-finalizer.R
+++ tests/testthat/test-finalizer.R
@@ -98,3 +98,265 @@ test_that("Finalizers have the right environment, non-portable #2", {
   expect_true(parenv$pub)
   expect_true(parenv$priv)
 })
+
+
+test_that("Finalizers are inherited, portable", {
+
+  AC <- R6Class(
+    "AC",
+    public = list(
+      finalize = function() print("An AC was just deleted")
+    )
+  )
+
+  BC <- R6Class(
+    "BC",
+    inherit = AC
+  )
+
+  B <- BC$new()
+  expect_output({ rm(B); gc() }, "An AC was just deleted")
+})
+
+
+test_that("Children can override finalizers, portable", {
+
+  AC <- R6Class(
+    "AC",
+    public = list(
+      finalize = function() cat("An AC was just deleted")
+    )
+  )
+
+  BC <- R6Class(
+    "BC",
+    inherit = AC,
+    public = list(
+      finalize = function() cat("A BC was just deleted")
+    )
+  )
+
+  B <- BC$new()
+  ## The anchors make sure that there is no extra output here
+  expect_output({ rm(B); gc() }, "^A BC was just deleted$")
+})
+
+
+test_that("Children can call finalizers in the parent, portable", {
+
+  AC <- R6Class(
+    "AC",
+    public = list(
+      finalize = function() cat("An AC was just deleted\n")
+    )
+  )
+
+  BC <- R6Class(
+    "BC",
+    inherit = AC,
+    public = list(
+      finalize = function() {
+        super$finalize()
+        cat("A BC was just deleted\n")
+      }
+    )
+  )
+
+  B <- BC$new()
+  expect_output(
+    { rm(B); gc() },
+    "An AC was just deleted.*A BC was just deleted"
+  )
+})
+
+
+test_that("Finalizers and two levels of inheritance, portable", {
+  AC <- R6Class(
+    "AC",
+    public = list(
+      finalize = function() cat("An AC was just deleted\n")
+    )
+  )
+
+  BC <- R6Class(
+    "BC",
+    inherit = AC,
+    public = list(
+      finalize = function() {
+        super$finalize()
+        cat("A BC was just deleted\n")
+      }
+    )
+  )
+
+  CC <- R6Class(
+    "CC",
+    inherit = BC,
+    public = list(
+      finalize = function() {
+        super$finalize()
+        cat("A CC was just deleted\n")
+      }
+    )
+  )
+
+  C <- CC$new()
+  expect_output(
+    { rm(C); gc() },
+    "An AC was just deleted.*A BC was just deleted.*A CC was just deleted"
+  )
+})
+
+
+test_that("Finalizers are inherited, non-portable", {
+
+  AC <- R6Class(
+    "AC",
+    public = list(
+      finalize = function() print("An AC was just deleted")
+    ),
+    portable = FALSE
+  )
+
+  BC <- R6Class(
+    "BC",
+    inherit = AC,
+    portable = FALSE
+  )
+
+  B <- BC$new()
+  expect_output({ rm(B); gc() }, "An AC was just deleted")
+})
+
+
+test_that("Children can override finalizers, non-portable", {
+
+  AC <- R6Class(
+    "AC",
+    public = list(
+      finalize = function() cat("An AC was just deleted")
+    ),
+    portable = FALSE
+  )
+
+  BC <- R6Class(
+    "BC",
+    inherit = AC,
+    public = list(
+      finalize = function() cat("A BC was just deleted")
+    ),
+    portable = FALSE
+  )
+
+  B <- BC$new()
+  ## The anchors make sure that there is no extra output here
+  expect_output({ rm(B); gc() }, "^A BC was just deleted$")
+})
+
+
+test_that("Children can call finalizers in the parent, non-portable", {
+
+  AC <- R6Class(
+    "AC",
+    public = list(
+      finalize = function() cat("An AC was just deleted\n")
+    ),
+    portable = FALSE
+  )
+
+  BC <- R6Class(
+    "BC",
+    inherit = AC,
+    public = list(
+      finalize = function() {
+        super$finalize()
+        cat("A BC was just deleted\n")
+      }
+    ),
+    portable = FALSE
+  )
+
+  B <- BC$new()
+  expect_output(
+    { rm(B); gc() },
+    "An AC was just deleted.*A BC was just deleted"
+  )
+})
+
+
+test_that("Finalizers and two levels of inheritance, portable", {
+  AC <- R6Class(
+    "AC",
+    public = list(
+      finalize = function() cat("An AC was just deleted\n")
+    )
+  )
+
+  BC <- R6Class(
+    "BC",
+    inherit = AC,
+    public = list(
+      finalize = function() {
+        super$finalize()
+        cat("A BC was just deleted\n")
+      }
+    )
+  )
+
+  CC <- R6Class(
+    "CC",
+    inherit = BC,
+    public = list(
+      finalize = function() {
+        super$finalize()
+        cat("A CC was just deleted\n")
+      }
+    )
+  )
+
+  C <- CC$new()
+  expect_output(
+    { rm(C); gc() },
+    "An AC was just deleted.*A BC was just deleted.*A CC was just deleted"
+  )
+})
+
+test_that("Finalizers and two levels of inheritance, non-portable", {
+  AC <- R6Class(
+    "AC",
+    public = list(
+      finalize = function() cat("An AC was just deleted\n")
+    ),
+    portable = FALSE
+  )
+
+  BC <- R6Class(
+    "BC",
+    inherit = AC,
+    public = list(
+      finalize = function() {
+        super$finalize()
+        cat("A BC was just deleted\n")
+      }
+    ),
+    portable = FALSE
+  )
+
+  CC <- R6Class(
+    "CC",
+    inherit = BC,
+    public = list(
+      finalize = function() {
+        super$finalize()
+        cat("A CC was just deleted\n")
+      }
+    ),
+    portable = FALSE
+  )
+
+  C <- CC$new()
+  expect_output(
+    { rm(C); gc() },
+    "An AC was just deleted.*A BC was just deleted.*A CC was just deleted"
+  )
+})
