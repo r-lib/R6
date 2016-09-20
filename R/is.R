9e1a18db9b0f9b89e0aa499c5c701befe7d42b3e
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2016-09-20 08:25:42 +0300

    Fixes to is* fns, as requested by @wch.

--- R/is.R
+++ R/is.R
@@ -3,37 +3,28 @@
 #' Checks for R6 class generators and R6 objects.
 #' @param x Any variable.
 #' @return A logical value.
-#' \code{is.r6} returns \code{TRUE} when the input is an R6 class generator or
-#' an R6 object, and \code{FALSE} otherwise.
-#' \code{is.r6gen} returns \code{TRUE} when the input is an R6 class generator
-#' and \code{FALSE} otherwise.
-#' \code{is.r6obj} returns \code{TRUE} when the input is an R6 object and
-#' \code{FALSE} otherwise.
+#' \itemize{
+#' \item{\code{is.R6Class} returns \code{TRUE} when the input is an R6 class
+#' generator and \code{FALSE} otherwise.}
+#' \item{\code{is.R6} returns \code{TRUE} when the input is an R6 object and
+#' \code{FALSE} otherwise.}
+#' }
 #' @examples
-#' class_generator <- R6::R6Class()
+#' class_generator <- R6Class()
 #' object <- class_generator$new()
-#' is.r6(class_generator)
-#' is.r6gen(class_generator)
-#' is.r6obj(class_generator)
-#' is.r6(object)
-#' is.r6gen(object)
-#' is.r6obj(object)
+#' is.R6Class(class_generator)
+#' is.R6(class_generator)
+#' is.R6Class(object)
+#' is.R6(object)
 #' @export
-is.r6 <- function(x)
-{
-  is.r6obj(x) || is.r6gen(x)
-}
-
 #' @rdname is.r6
 #' @export
-is.r6obj <- function(x)
-{
+is.R6 <- function(x) {
   inherits(x, "R6")
 }
 
 #' @rdname is.r6
 #' @export
-is.r6gen <- function(x)
-{
+is.R6Class <- function(x) {
   inherits(x, "R6ClassGenerator")
 }
