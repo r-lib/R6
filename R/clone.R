c03402e19a326320406ce2a5f4cac284a0cb2253
Winston Chang <winston@stdout.org>
2018-09-12 22:30:12 -0500

    Refactor cloning code

--- R/clone.R
+++ R/clone.R
@@ -29,84 +29,48 @@ generator_funs$clone_method <- function(deep = FALSE) {
     list2env(x, envir)
   }
 
-  clone_super <- function(old_enclos_env, new_enclos_env, public_bind_env,
-                          has_private, private_bind_env)
-  {
-    old_super_bind_env <- old_enclos_env$super
-    if (is.null(old_super_bind_env))
-      return()
+  # ---------------------------------------------------------------------------
+  # Create representation of the old object
+  # ---------------------------------------------------------------------------
+  old <- list(
+    list(
+      enclosing = .subset2(self, ".__enclos_env__"),
+      binding   = self,      # AKA the public binding environment
+      private   = NULL
+    )
+  )
 
-    # Copy all the methods from the old super binding env to the new one, and
-    # set their enclosing env to a new one.
-    super_copies <- as.list.environment(old_super_bind_env, all.names = TRUE)
-    super_copies <- super_copies[setdiff(names(super_copies), ".__enclos_env__")]
-
-    # Degenerate case: super env is empty
-    if (length(super_copies) == 0) {
-      new_enclos_env$super <- new.env(parent = emptyenv(), hash = FALSE)
-      return()
-    }
-
-    # Get the enclosing env for the super object. Note that this is the
-    # enclosing env only for methods declared for this level of super object --
-    # if this super object has inherited methods from its super object, then
-    # those methods will have a different environment. (#119)
-    old_super_enclos_env <- old_super_bind_env$.__enclos_env__
-
-    # Create new super enclos env and populate with self and private.
-    new_super_enclos_env <- new.env(parent = parent.env(old_super_enclos_env),
-                                    hash = FALSE)
-    new_super_enclos_env$self <- public_bind_env
-    if (has_private)
-      new_super_enclos_env$private <- private_bind_env
-
-    new_super_bind_env <- new.env(parent = emptyenv(), hash = FALSE)
-    new_super_bind_env$.__enclos_env__ <- new_super_enclos_env
-
-    # Fix up environments for methods
-    super_copies <- assign_func_envs(super_copies, new_super_enclos_env)
-
-    # Separate active from non-active items
-    active_idx <- vapply(names(super_copies), bindingIsActive, env = old_super_bind_env,
-                         TRUE)
-    active_copies     <- super_copies[active_idx]
-    non_active_copies <- super_copies[!active_idx]
-
-    # Copy over items
-    list2env2(non_active_copies, new_super_bind_env)
-
-    if (length(active_copies) > 0) {
-      for (name in names(active_copies)) {
-        makeActiveBinding(name, active_copies[[name]], new_super_bind_env)
-      }
-    }
-
-    new_enclos_env$super <- new_super_bind_env
-
-    # Recurse
-    clone_super(old_super_enclos_env, new_super_enclos_env, public_bind_env,
-                has_private, private_bind_env)
-  }
-
-  # ------------------------------------------------------------------
-
-  old_enclos_env <- .subset2(self, ".__enclos_env__")
-  if (!is.environment(old_enclos_env)) {
+  if (!is.environment(old[[1]]$enclosing)) {
     stop("clone() must be called from an R6 object.")
   }
 
-  old_public_bind_env <- self
-  old_private_bind_env <- old_enclos_env$private
-  has_private <- !is.null(old_private_bind_env)
+  old[[1]]$private <- old[[1]]$enclosing$private
+  has_private <- !is.null(old[[1]]$private)
 
   # Figure out if we're in a portable class object
-  portable <- !identical(old_public_bind_env, old_enclos_env)
+  portable <- !identical(old[[1]]$binding, old[[1]]$enclosing)
+
+  # Traverse the super binding and enclosing environments, and add them to the
+  # list.
+  i <- 1
+  while (TRUE) {
+    if (is.null(old[[i]]$enclosing$super)) {
+      break
+    }
+
+    old[[i+1]] <- list(
+      binding   = old[[i]]$enclosing$super,
+      enclosing = old[[i]]$enclosing$super$.__enclos_env__
+    )
+
+    i <- i + 1
+  }
 
   # Set up stuff for deep clones
   if (deep) {
-    if (has_private && is.function(old_private_bind_env$deep_clone)) {
+    if (has_private && is.function(old[[1]]$private$deep_clone)) {
       # Get private$deep_clone, if available.
-      deep_clone <- old_private_bind_env$deep_clone
+      deep_clone <- old[[1]]$private$deep_clone
     } else {
       # If there's no private$deep_clone, then this default function will copy
       # fields that are R6 objects.
@@ -120,107 +84,193 @@ generator_funs$clone_method <- function(deep = FALSE) {
     }
   }
 
-  # Create the new binding and enclosing environments
-  if (portable) {
-    if (has_private) {
-      private_bind_env <- new.env(emptyenv(), hash = FALSE)
-    }
-    public_bind_env <- new.env(emptyenv(), hash = FALSE)
-    new_enclos_env <- new.env(parent.env(old_enclos_env), hash = FALSE)
+  # ---------------------------------------------------------------------------
+  # Create representation of the new object
+  # ---------------------------------------------------------------------------
 
-  } else {
-    if (has_private) {
-      private_bind_env <- new.env(parent.env(old_private_bind_env), hash = FALSE)
-      public_bind_env <- new.env(private_bind_env, hash = FALSE)
+  # The object representation is made up of a list of "slices". Each slice
+  # represents one level of inheritance. The first slice is somewhat different
+  # from subsequent ones. The later ones are superclass slices. They do not
+  # have a separate `private` environment.
+
+  # Create the first slice. This one is different from the others.
+  make_first_new_slice <- function(old_slice, portable) {
+    new_slice <- list(
+      enclosing = NULL,
+      binding   = NULL
+    )
+
+    has_private <- !is.null(old_slice$private)
+
+    if (portable) {
+      enclosing_parent <- parent.env(old_slice$enclosing)
+      binding_parent   <- emptyenv()
+
+      if (has_private) {
+        private_parent   <- emptyenv()
+        new_slice$private <- new.env(private_parent, hash = FALSE)
+      }
+      new_slice$binding   <- new.env(binding_parent,   hash = FALSE)
+      new_slice$enclosing <- new.env(enclosing_parent, hash = FALSE)
+
     } else {
-      public_bind_env <- new.env(parent.env(old_public_bind_env), hash = FALSE)
+      if (has_private) {
+        private_parent   <- parent.env(old_slice$private)
+        new_slice$private <- new.env(private_parent, hash = FALSE)
+
+        binding_parent   <- new_slice$private
+        new_slice$binding <- new.env(binding_parent, hash = FALSE)
+
+      } else {
+        binding_parent <- parent.env(old_slice$binding)
+        new_slice$binding <- new.env(binding_parent, hash = FALSE)
+      }
+
+      new_slice$enclosing <- new_slice$binding
     }
-    new_enclos_env <- public_bind_env
+
+    # Set up self, private, and .__enclos_env__
+    new_slice$enclosing$self <- new_slice$binding
+    if (has_private) {
+      new_slice$enclosing$private <- new_slice$private
+    }
+    new_slice$binding$.__enclos_env__ <- new_slice$enclosing
+
+    new_slice
   }
 
 
-  # Copy members ----------------------------------------------------
+  # This creates a slice other than the first one.
+  make_new_slice <- function(old_slice, self, private) {
+    enclosing_parent <- parent.env(old[[i]]$enclosing)
+    binding_parent   <- parent.env(old[[i]]$binding)
 
-  # Copy the old objects, fix up method environments, and put them into the
-  # new binding environment.
-  public_copies <- as.list.environment(old_public_bind_env, all.names = TRUE)
-  # If non-portable, `self` will be there; make sure not to copy it.
-  if (!portable) {
-    public_copies$self <- NULL
-  }
-  # Don't copy .__enclos_env__
-  public_copies <- public_copies[setdiff(names(public_copies), ".__enclos_env__")]
-  public_copies <- assign_func_envs(public_copies, new_enclos_env)
+    enclosing <- new.env(enclosing_parent, hash = FALSE)
+    binding   <- new.env(binding_parent,   hash = FALSE)
 
-  # Separate active and non-active bindings
-  active_idx <- vapply(names(public_copies), bindingIsActive, env = old_public_bind_env,
-                       logical(1))
-  active_copies <- public_copies[active_idx]
-  public_copies <- public_copies[!active_idx]
+    enclosing$self <- self
+    if (!is.null(private)) {
+      enclosing$private <- private
+    }
 
-  if (deep) {
-    public_copies <- mapply(deep_clone, names(public_copies), public_copies,
-                            SIMPLIFY = FALSE)
+    binding$.__enclos_env__ <- enclosing
+
+    list(
+      enclosing = enclosing,
+      binding = binding
+    )
   }
 
-  # Copy in public and active bindings
-  list2env2(public_copies, public_bind_env)
+  new <- list(
+    make_first_new_slice(old[[1]], portable)
+  )
 
-  if (length(active_copies) > 0) {
-    for (name in names(active_copies)) {
-      makeActiveBinding(name, active_copies[[name]], public_bind_env)
+  # Mirror the super environments from the old object
+  if (length(old) > 1) {
+    for (i in seq(2, length(old))) {
+      new[[i]] <- make_new_slice(
+        old[[i]],
+        new[[1]]$binding,
+        new[[1]]$private
+      )
+    }
+
+    # A second pass to add in the `super` to each enclosing environment.
+    for (i in seq(1, length(old)-1)) {
+      new[[i]]$enclosing$super <- new[[i+1]]$binding
     }
   }
 
-  # Copy private members
-  if (has_private) {
-    private_copies <- as.list.environment(old_private_bind_env, all.names = TRUE)
+  # ---------------------------------------------------------------------------
+  # Copy members from old to new
+  # ---------------------------------------------------------------------------
+  copy_slice <- function(old_slice, new_slice) {
+    # Copy the old objects, fix up method environments, and put them into the
+    # new binding environment.
+    binding_copies <- as.list.environment(old_slice$binding, all.names = TRUE)
+
+    # Don't copy self, private, super, or .__enclos_env__
+    binding_copies <- binding_copies[
+      setdiff(names(binding_copies), c("self", "private", "super", ".__enclos_env__"))
+    ]
+
+    # TODO: Fix this up to iterate over super envs
+    binding_copies <- assign_func_envs(binding_copies, new_slice$enclosing)
+
+    # Separate active and non-active bindings
+    active_idx <- vapply(
+      names(binding_copies),
+      bindingIsActive,
+      env = old_slice$binding,
+      TRUE
+    )
+    active_copies  <- binding_copies[active_idx]
+    binding_copies <- binding_copies[!active_idx]
+
     if (deep) {
-      private_copies <- mapply(deep_clone, names(private_copies), private_copies,
-                               SIMPLIFY = FALSE)
+      binding_copies <- mapply(
+        deep_clone,
+        names(binding_copies),
+        binding_copies,
+        SIMPLIFY = FALSE
+      )
     }
-    private_copies <- assign_func_envs(private_copies, new_enclos_env)
-    list2env2(private_copies, private_bind_env)
+
+    # Copy in public and active bindings
+    list2env2(binding_copies, new_slice$binding)
+
+    if (length(active_copies) > 0) {
+      for (name in names(active_copies)) {
+        makeActiveBinding(name, active_copies[[name]], new_slice$binding)
+      }
+    }
+
+    # Copy private members
+    if (!is.null(old_slice$private)) {
+      private_copies <- as.list.environment(old_slice$private, all.names = TRUE)
+      if (deep) {
+        private_copies <- mapply(
+          deep_clone,
+          names(private_copies),
+          private_copies,
+          SIMPLIFY = FALSE
+        )
+      }
+      private_copies <- assign_func_envs(private_copies, new_slice$enclosing)
+      list2env2(private_copies, new_slice$private)
+    }
   }
 
-  # Clone super object -------------------------------------------
-  clone_super(old_enclos_env, new_enclos_env, public_bind_env, has_private,
-              private_bind_env)
+  for (i in seq_along(old)) {
+    # This works because the objects in new are reference objects
+    copy_slice(old[[i]], new[[i]])
+  }
 
-  # Add refs to other environments in the object --------------------
-  public_bind_env$`.__enclos_env__` <- new_enclos_env
 
-  # Add self and (optional) private pointer ---------------------------
-  new_enclos_env$self <- public_bind_env
-  if (has_private)
-    new_enclos_env$private <- private_bind_env
-
-  class(public_bind_env) <- class(old_public_bind_env)
+  class(new[[1]]$binding) <- class(old[[1]]$binding)
 
   # Lock --------------------------------------------------------------
   # Copy locked state of environment
-  if (environmentIsLocked(old_public_bind_env)) {
-    lockEnvironment(public_bind_env)
+  if (environmentIsLocked(old[[1]]$binding)) {
+    lockEnvironment(new[[1]]$binding)
   }
-  if (has_private && environmentIsLocked(old_private_bind_env)) {
-    lockEnvironment(private_bind_env)
+  if (has_private && environmentIsLocked(old[[1]]$private)) {
+    lockEnvironment(new[[1]]$private)
   }
 
   # Always lock methods
-  # We inspect the names in public_copies instead public_bind_env, because
-  # ls() is so slow for environments. R 3.2.0 introduced the sorted=FALSE
-  # option, which makes ls() much faster, so at some point we'll be able to
-  # switch to that.
-  for (name in names(public_copies)) {
-    if (is.function(.subset2(public_bind_env, name)))
-      lockBinding(name, public_bind_env)
+  # R 3.2.0 introduced the sorted=FALSE option, which makes ls() much faster,
+  # so at some point we'll be able to switch to that.
+  for (name in ls(new[[1]]$binding)) {
+    if (is.function(.subset2(new[[1]]$binding, name)))
+      lockBinding(name, new[[1]]$binding)
   }
   if (has_private) {
-    for (name in names(private_copies)) {
-      if (is.function(private_bind_env[[name]]))
-        lockBinding(name, private_bind_env)
+    for (name in names(new[[1]]$private)) {
+      if (is.function(new[[1]]$private[[name]]))
+        lockBinding(name, new[[1]]$private)
     }
   }
 
-  public_bind_env
+  new[[1]]$binding
 }
